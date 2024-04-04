93ae41131b983afb4df4da909329b586c728971c
rockandfire <jackson.wman@gmail.com>
2024-04-04 18:24:11 +0000

    create gitignore, update script to create local db, rm test db from branch

--- django-quickstart.sh
+++ django-quickstart.sh
@@ -5,4 +5,15 @@ pip install --upgrade pip
 pip install django-cms djangocms-text-ckeditor djangocms-frontend django-filer djangocms-versioning djangocms-alias djangocms_admin_style
 
 cd capstonecms
+
+export EXISTS=$(ls . | grep db.sqlite3)
+if [ $EXISTS = 'db.sqlite3' ]; then
+	echo 'DATABASE EXISTS, CONTINUING'
+else 
+	echo 'DATABASE DOES NOT EXIST. CREATING NEW DATABASE...'
+	python3 -m manage migrate
+	python3 -m manage createsuperuser
+	python3 -m manage cms check
+fi
+
 python -m manage runserver
